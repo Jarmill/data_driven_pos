dfc18b095ca28c75088f868304ed0678807157c0
Jared Miller <jared.fr.miller@gmail.com>
2022-11-15 14:59:29 -0500

    Buffer tests and (C, D) LQR parameters
    Example 1 of  https://ieeexplore.ieee.org/document/8618689 works, and involves parameters C = [I; 0] and D = [0; I] (like in LQR). This example has a case where A is stable to begin with.
    
    This same choice of parameters (C, D) fail when applied to a the test system p2p_clean_cont.m in which A is unstable.

--- testing/p2p_buffer_test.m
+++ testing/p2p_buffer_test.m
@@ -1,16 +1,9 @@
-rng(30, 'twister')
-    % xdot = A x + B u + E w
-    % z    = C x + D u + F w
-    %peak-to-peak w->z
-    
-n = 3;
-m = 2; %feasible
+%Example 1 of https://ieeexplore.ieee.org/document/8618689
+A = diag(-[1,2,3,4]);
+B = [-1 1 0 0 0 0; 0 -1 -1 1 0 0; 1 0 1 -1 -1 1; 0 0 0 0 1 -1];
 
-sys = struct('A', [-0.562888446377643,0.304206397381707,0.656593915356461;0.062043810844475,-1.344996926420559,0.254522592715944;0.113187181708781,0.132354437133599,0.019971221719390],...
-             'B', [0.182453070500558,0.076396428548559;0.472833184316772,0.245344456124525;0.065942821511921,0.940264967809949]);
-
-%this is unstable at the current settings
-es = eig(sys.A);
+n = 4; 
+m = 6;
 
 
 %% Declare Variables
@@ -19,11 +12,13 @@ delta = 1e-3;
 
 %design
 y = sdpvar(n, 1);
-S = sdpvar(m, n);
+% S = sdpvar(m, n);
+s = sdpvar(m, 1);
+S = blkdiag(s(1), s(2:3), s(4:5), s(6));
 gamma = sdpvar(1, 1);
 %parameters of p2p problem
 
-p = 3; %number of external inputs
+p = 4; %number of external inputs
 % q=n+m; %number of controlled outputs
 q = n+m;
 C = [eye(n);zeros(m, n)];
@@ -33,7 +28,7 @@ D = [zeros(n, m); eye(m)];
 % C = rand(q, n);
 % D = zeros(q, m);
 % D = [zeros(n, m); eye(m)*0.5];
-E = eye(n, p);
+E = eye(p);
 F = zeros(q, p);
 
 %% form constraints
@@ -45,7 +40,7 @@ X = diag(y);
 
 %condition (6.2) of https://ieeexplore.ieee.org/document/8618689
 %equation (12) of https://arxiv.org/pdf/1204.3554.pdf
-Abar = (sys.A*X + sys.B*S);
+Abar = (A*X + B*S);
 Cbar = (C*X + D*S);
 term_x = -Abar *ones(n, 1) - E*ones(p, 1);
 term_z = gamma*ones(q, 1) - Cbar*ones(n, 1) - F*ones(p, 1);
@@ -73,10 +68,10 @@ else
     K_rec = S_rec*diag(v_rec);
 
 
-    sys_clp = sys.A + sys.B*K_rec
+    sys_clp = A + B*K_rec
     e_clp = eig(sys_clp)'
     gamma_rec = value(gamma)
     
     out_clean = struct('y', y_rec, 'v', v_rec, 'S', S_rec, 'K', K_rec,...
-        'A', sys.A, 'B', sys.B, 'gamma', gamma);
+        'A', A, 'B', B, 'gamma', gamma);
 end
