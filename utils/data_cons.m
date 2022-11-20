5ffbed64d425b609ed7ed6d13553bc4823cff677
Jared Miller <jared.fr.miller@gmail.com>
2022-11-19 21:12:09 -0500

    A more efficient way to generate the consistency polytope
    no more for loop
    
    negligible performance boost, but good for deriving the math in the paper

--- utils/data_cons.m
+++ utils/data_cons.m
@@ -1,4 +1,4 @@
-function [C, d] = data_cons(traj, nontrivial)
+function [C2, d2] = data_cons(traj, nontrivial)
     %DATA_CONS generate the Linf-polytope constraints associated
     %with the data in traj
     %
@@ -34,33 +34,42 @@ function [C, d] = data_cons(traj, nontrivial)
         pa = reshape([A,B], [], 1);
     end
     
-    for t = 1:T
-        xcurr = traj.Xn(:, t);
-        xnext = traj.Xdelta(:, t);
-        ucurr = traj.U(:, t);
-
-        Amat = kron(xcurr', eye(n));
-        Bmat = kron(ucurr', eye(n));
-        
-        dcurr = epsilon + kron([1;-1], xnext);
-        blkcurr = [Amat, Bmat; -Amat, -Bmat];
-        
-
-%         dcurr = epsilon + xnext;
-%         blkcurr = [Amat, Bmat];
-        
-        C(ind + (1:(2*n)), :) = blkcurr;
-        
-        d(ind + (1:(2*n))) = dcurr;
-
-        ind = ind + (2*n);
-    end
+%     for t = 1:T
+%         xcurr = traj.Xn(:, t);
+%         xnext = traj.Xdelta(:, t);
+%         ucurr = traj.U(:, t);
+% 
+%         Amat = kron(xcurr', eye(n));
+%         Bmat = kron(ucurr', eye(n));
+%         
+%         dcurr = epsilon + kron([1;-1], xnext);
+%         blkcurr = [Amat, Bmat; -Amat, -Bmat];
+%         
+% 
+% %         dcurr = epsilon + xnext;
+% %         blkcurr = [Amat, Bmat];
+%         
+%         C(ind + (1:(2*n)), :) = blkcurr;
+%         
+%         d(ind + (1:(2*n))) = dcurr;
+% 
+%         ind = ind + (2*n);
+%     end
+    
+    Amat2 = kron(traj.Xn', eye(n));
+    Bmat2 = kron(traj.U', eye(n));
+    
+    C2=  [Amat2, Bmat2; -Amat2, -Bmat2];
+    
+    d2 = epsilon + kron([1;-1],reshape(traj.Xdelta, [], 1));
+    
     
     if nontrivial    
-        [C_red, d_red] = nontrivial_constraints(C, d);
-        poly = struct('A', C_red, 'b', d_red, 'F_old', length(d), 'F_new', length(d_red));
+%         [C_red, d_red] = nontrivial_constraints(C, d);
+        [C2, d2] = nontrivial_constraints(C2, d2);
+        poly = struct('A', C2, 'b', d2, 'F_old', length(d), 'F_new', length(d_red));
     else
-        poly = struct('A', C, 'b', d);
+        poly = struct('A', C2, 'b', d2);
     end        
     
 
