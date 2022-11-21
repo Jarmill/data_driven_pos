6beb6d236f6f98f1f43c4defc424ac6472799e54
Jared Miller <jared.fr.miller@gmail.com>
2022-11-21 13:37:31 -0500

    Can constrain positivity of ground-truth systems through data_opts
    It is no longer required that the ground truth is (internally) positive.

---
+++ class_farkas/data_opts.m
@@ -0,0 +1,13 @@
+classdef data_opts
+    %DATA_OPTS Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        nontrivial = 1;
+        pos_A = 1;
+        pos_B = 1;
+    end
+    
+
+end
+
