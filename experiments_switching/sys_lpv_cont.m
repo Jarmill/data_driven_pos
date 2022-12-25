efe74d2cc81fe56cfd4762256af1ad26e60f7fd5
Jared Miller <jared.fr.miller@gmail.com>
2022-12-25 18:52:10 -0500

    LPV cont experiment and image

--- experiments_switching/sys_lpv_cont.m
+++ experiments_switching/sys_lpv_cont.m
@@ -10,7 +10,6 @@ m =2;
 L = 3;
 
 T = 10; %works
-% T = 20; %works
 
 Th_vert = [ 1  1 1 1;
            -1 -1 1 1;
@@ -19,24 +18,11 @@ Th_vert = [ 1  1 1 1;
 th_scale = [1; 1; 0.7];
 th_trans = [0; 0 ;0.2];
 Th_vert = diag(th_scale)*Th_vert + th_trans;
-% %does not work
-% rng(50, 'twister')
-% n= 3;
-% m =2;
-% L = 3;
-% 
-% Th_vert = [ 1  1 1 1;
-%            -1 -1 1 1;
-%            1  -1 1 -1];
-% Th_vert = diag([1; 0; 0.1])*Th_vert + [0; 0; 0.5];
-
-% Th_vert = diag([0.5; 0.5])*Th_vert + [1; 1];
-
-
 
 
 Nv = size(Th_vert, 2);
 
+%% sample a trajectory 
 PS = possim_lpv_cont(n, m, 0.1, L);
 
 PS.sampler.th = @() [1;  (th_scale(2:3)).*(2*rand(PS.L-1,1)-1) + th_trans(2:3)];
@@ -49,6 +35,7 @@ traj = PS.sim(T, sys);
 dopts = data_opts;
 ST = posstab_lpv_cont_f(traj, Th_vert, dopts);
 
+%check if the plant is inside the polytope (bug-check)
 sys_combined = [];
 for i = 1:L
     sys_combined = [sys_combined, sys.A{i}];
@@ -147,7 +134,7 @@ if PLOT
         end
     end
     scatter(x0(1), x0(2), 300, 'ok')
-%     xlim([x0(1)-0.1, 0.1])
+
     xlabel('$x_1$', 'interpreter', 'latex')
     ylabel('$x_2$', 'interpreter', 'latex')
     title('1 Parameter Sequence', 'FontSize', 16)
@@ -161,7 +148,7 @@ if PLOT
     end
     scatter(x0(1), x0(2), 300, 'ok')
 title(sprintf('%d Parameter Sequences', Ntraj), 'FontSize', 16)
-% xlim([x0(1)-0.1, 0.1])
+
 xlabel('$x_1$', 'interpreter', 'latex')
 ylabel('$x_2$', 'interpreter', 'latex')
 end
